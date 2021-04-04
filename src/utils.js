ad1b9f80184d43d1dcabe03407bc4060931db32a
dwightjack <marco.solazzi@gmail.com>
2021-04-04 17:56:24 +0900

    use free api service

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,12 @@
 import ky from 'ky';
 export async function fetchRates(base) {
-  if (fetchRates.$$store.has(base)) {
-    return fetchRates.$$store.get(base);
-  }
-
-  const call = ky
-    .get(`https://api.exchangeratesapi.io/latest?base=${base}`)
+  return ky
+    .get(`https://api.exchangerate.host/latest?base=${base}`)
     .json()
-    .then(({ rates }) => rates);
-
-  fetchRates.$$store.set(base, call);
-
-  const rates = await call;
-
-  fetchRates.$$store.delete(base);
-
-  return rates;
+    .then(({ rates, success }) => {
+      if (success === true) {
+        return rates;
+      }
+      throw new Error('Unable to retrieve rates.');
+    });
 }
-
-fetchRates.$$store = new Map();
