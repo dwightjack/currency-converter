8ce9e0ee055acf2f997a2dc76c042e194ef2db93
Marco Solazzi <marco.solazzi@gmail.com>
2021-05-29 19:06:24 +0900

    selectable currencies

--- src/stores/currency.ts
+++ src/stores/currency.ts
@@ -11,25 +11,22 @@ export const currency = writable({
 export const exchangeRate = derived(
   currency,
   ($currency, set) => {
-    fetchCurrency($currency.input).then(({ rates = {}}) => {
+    fetchCurrency($currency.input).then(({ rates = {} }) => {
       set(rates[$currency.output] || 0);
     });
   },
   0,
 );
 
-export const currencyList = readable(['JPY', 'EUR'], async (set) => {
-  const { symbols } = await fetchCurrencyList();
-  set(symbols);
-
+export const currencyList = readable(['JPY', 'EUR'], (set) => {
+  fetchCurrencyList().then(({ symbols }) => set(symbols));
   return () => {};
 });
 
 export const convertedAmount = derived(
   [inputAmount, exchangeRate, currency],
   ([$inputAmount, $exchangeRate, $currency]) => {
-    const converted =
-      ($inputAmount || 0) * ($exchangeRate);
+    const converted = ($inputAmount || 0) * $exchangeRate;
     return new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: $currency.output,
@@ -46,6 +43,13 @@ export function invertCurrency() {
   currency.update(({ input, output }) => ({
     input: output,
     output: input,
+  }));
+}
+
+export function setCurrency(key: 'input' | 'output', value: string) {
+  currency.update((val) => ({
+    ...val,
+    [key]: value,
   }));
 }
 
