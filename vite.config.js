ea8af342d81685d109eb3acddcf4998d1d9546a2
dwightjack <marco.solazzi@gmail.com>
2023-04-13 14:42:30 +0900

    update unocss to latest setup

--- vite.config.js
+++ vite.config.js
@@ -3,51 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 import { VitePWA } from 'vite-plugin-pwa';
 import Unocss from 'unocss/vite';
-import presetWind from '@unocss/preset-wind';
-import presetIcons from '@unocss/preset-icons';
-import { extractorSvelte } from '@unocss/core';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
-import { theme } from './config/theme';
-
-function range(size, startAt = 0) {
-  return Array.from(Array(size).keys()).map((i) => i + startAt);
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    Unocss({
-      presets: [presetWind(), presetIcons()],
-      transformers: [transformerVariantGroup()],
-      extractors: [extractorSvelte],
-      safelist: [
-        ...range(20).map((i) => `gap-${i}`),
-        ...range(20).map((i) => `gap-x-${i}`),
-        ...range(20).map((i) => `gap-y-${i}`),
-        ...['center', 'stretch', 'baseline', 'start', 'end'].map(
-          (v) => `items-${v}`,
-        ),
-      ],
-      theme,
-      shortcuts: {
-        'outline-brand':
-          'outline-none focus-visible:outline-brand-600 @dark:focus-visible:outline-brand-dark-400',
-        'outline-brand-within':
-          'outline-none focus-within:outline-brand-600 @dark:focus-within:outline-brand-dark-400',
-      },
-      rules: [
-        [/^aspect-(\d+\/\d+)$/, ([_, ratio]) => ({ 'aspect-ratio': ratio })],
-        [
-          /^bg-image-\$([a-z][a-z-]*)$/,
-          ([_, varName]) => ({ 'background-image': `var(--${varName})` }),
-        ],
-        [
-          /^grid-area-\$([a-z][a-z-]*)$/,
-          ([_, varName]) => ({ 'grid-area': `var(--${varName})` }),
-        ],
-        [/^grid-area-\[([^\]]+)\]$/, ([_, area]) => ({ 'grid-area': area })],
-      ],
-    }),
+    Unocss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
