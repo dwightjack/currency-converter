205b870b99c0d88bfa948da5977b20f7746e79c2
dwightjack <marco.solazzi@gmail.com>
2023-04-14 17:48:46 +0900

    update pwa setting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import App from './App.svelte';
 import '@unocss/reset/tailwind.css';
 import 'virtual:uno.css';
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({ immediate: true });
 
 new App({
   target: document.body,
