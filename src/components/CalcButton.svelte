d036ea3829ae1df9d3bc63de2db72ec5dec25733
dwightjack <marco.solazzi@gmail.com>
2024-07-04 15:59:56 +0900

    state style

--- src/components/CalcButton.svelte
+++ src/components/CalcButton.svelte
@@ -5,11 +5,11 @@
 
   const themes: Record<typeof theme, string> = {
     light:
-      'bg-surface text-brand-900 active:bg-brand-200 @dark:(bg-surface-dark text-brand-dark-200 )',
+      'bg-surface text-brand-900 hover:bg-brand-100/40 active:bg-brand-200 @dark:(bg-surface-dark text-brand-dark-200 hover:bg-brand-dark-900 active:bg-brand-dark-900/50)',
     invert:
-      'bg-brand-800 text-surface active:bg-brand-900 @dark:(text-brand-dark-200 bg-brand-dark-700)',
+      'bg-brand-800 text-surface hover:bg-brand-700 active:bg-brand-900 @dark:(text-brand-dark-200 bg-brand-dark-700 active:bg-brand-dark-800 hover:bg-brand-dark-600)',
     neutral:
-      'bg-brand-100 text-brand-800 active:bg-brand-200 @dark:(text-brand-dark-200 bg-brand-dark-900)',
+      'bg-brand-100 text-brand-800 hover:bg-brand-100/80 active:bg-brand-200 @dark:(text-brand-dark-200 bg-brand-dark-900 active:bg-brand-dark-900/70 hover:bg-brand-dark-800)',
   };
 </script>
 
@@ -20,6 +20,6 @@
   aria-pressed={pressed}
   class="{themes[
     theme
-  ]} grid-area-$btn-area aria-pressed:(border-2 border-brand-900/70) ring-brand-200 @dark:(ring-brand-dark-700 active:bg-brand-dark-600) outline-brand focus-visible:(isolate) outline-offset-2 flex items-center justify-center ring-1 text-2xl"
+  ]} grid-area-$btn-area aria-pressed:(border-2 border-brand-900/70) ring-brand-200 @dark:(ring-brand-dark-700 aria-pressed:border-brand-dark-500) outline-brand focus-visible:(isolate) outline-offset-2 flex items-center justify-center ring-1 text-2xl"
   ><slot /></button
 >
--- src/components/ControlButton.svelte
+++ src/components/ControlButton.svelte
@@ -7,7 +7,7 @@
   const themes: Record<typeof theme, string> = {
     blue: 'text-brand-900 hover:bg-brand-100 active:bg-brand-200 outline-brand @dark:(text-brand-dark-200 hover:bg-brand-dark-700 active:bg-brand-dark-900)',
     green:
-      'text-success-700 active:bg-success-100 focus-visible:outline-success-500 @dark:(text-success active:bg-success-900)',
+      'text-success-700 hover:bg-success-100 active:bg-success-200 focus-visible:outline-success-500 @dark:(text-success hover:bg-success-700/70 active:bg-success-900)',
   };
 </script>
 
