5ea27f8a222db5d872e2afdc52a0d4f8e67ec649
dwightjack <marco.solazzi@gmail.com>
2024-07-04 10:33:23 +0900

    states

--- src/components/CalcButton.svelte
+++ src/components/CalcButton.svelte
@@ -5,11 +5,11 @@
 
   const themes: Record<typeof theme, string> = {
     light:
-      'bg-surface text-brand-900 active:bg-brand-100 @dark:(bg-surface-dark text-brand-dark-200 )',
+      'bg-surface text-brand-900 active:bg-brand-200 @dark:(bg-surface-dark text-brand-dark-200 )',
     invert:
-      'bg-brand-800 text-surface active:bg-brand-800 @dark:(text-brand-dark-200 bg-brand-dark-700)',
+      'bg-brand-800 text-surface active:bg-brand-900 @dark:(text-brand-dark-200 bg-brand-dark-700)',
     neutral:
-      'bg-brand-100 text-brand-800 active:bg-brand-100 @dark:(text-brand-dark-200 bg-brand-dark-900)',
+      'bg-brand-100 text-brand-800 active:bg-brand-200 @dark:(text-brand-dark-200 bg-brand-dark-900)',
   };
 </script>
 
@@ -18,6 +18,8 @@
   type="button"
   style={`--btn-area: ${area}`}
   aria-pressed={pressed}
-  class={`${themes[theme]} grid-area-$btn-area aria-pressed:(shadow shadow-brand-600 shadow-inset) ring-brand-200 @dark:(ring-brand-dark-700 active:bg-brand-dark-600) outline-brand focus-visible:(isolate) outline-offset-2 flex items-center justify-center ring-1 text-2xl`}
+  class="{themes[
+    theme
+  ]} grid-area-$btn-area aria-pressed:(border-2 border-brand-900/70) ring-brand-200 @dark:(ring-brand-dark-700 active:bg-brand-dark-600) outline-brand focus-visible:(isolate) outline-offset-2 flex items-center justify-center ring-1 text-2xl"
   ><slot /></button
 >
