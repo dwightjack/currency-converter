629331bf5c53da1fe114274d7bdae6a3a5032d02
dwightjack <marco.solazzi@gmail.com>
2025-12-11 18:28:09 +0900

    make box fill the screen on mobile

--- src/components/CurrencyBox.svelte
+++ src/components/CurrencyBox.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <fieldset
-  class="p-block-0 p-inline-0 border-2 border-brand-200 text-xl rounded-md min-inline-0 @dark:(border-brand-dark-700)"
+  class="p-block-0 inline-full p-inline-0 border-2 border-brand-200 text-xl rounded-md min-inline-0 @dark:(border-brand-dark-700)"
 >
   <legend class="sr-only">{label}</legend>
   {@render select(current)}
